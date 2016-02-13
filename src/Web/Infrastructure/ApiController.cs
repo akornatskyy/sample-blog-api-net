46952679ea8babd1d30bf2170f94937a834d4ff5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-02-13 16:11:46 +0200

    Changed visibility of GetService method to protected.

--- src/Web/Infrastructure/ApiController.cs
+++ src/Web/Infrastructure/ApiController.cs
@@ -4,7 +4,7 @@ namespace Blog.Web.Infrastructure
 {
     public class ApiController : System.Web.Http.ApiController
     {
-        public T GetService<T>()
+        protected T GetService<T>()
         {
             return (T)this.Request.GetDependencyScope().GetService(typeof(T));
         }
